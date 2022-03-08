74544f72d4aef7cfb620851cd5033a264e862f0f
Andre <andrestone@users.noreply.github.com>
2022-03-08 19:49:57 +0100

    fix: validating remote member registration

---
+++ src/commands/registerRemoteMember.test.ts
@@ -0,0 +1,62 @@
+import { BotContext } from '../context';
+import { cmdRegisterRemoteMember } from './registerRemoteMember';
+
+type DeepPartial<T> = T extends object ? {
+    [P in keyof T]?: DeepPartial<T[P]>;
+} : T;
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+});
+
+it('replies with syntax error when command is issued with single location', async () => {
+  // Prepare
+  const tMock = jest.fn().mockImplementation(() => 'dummy');
+  const replyWithAutoDestructiveMessageMock = jest.fn();
+  const ctx: DeepPartial<BotContext> = {
+    safeUser: { mention: undefined },
+    command: {},
+    i18n: { t: tMock },
+    replyWithAutoDestructiveMessage: replyWithAutoDestructiveMessageMock,
+  };
+
+  // Given
+  ctx.command = { args: 'BR' };
+  ctx.safeUser = { mention: 'oi' };
+
+  // When
+  await (cmdRegisterRemoteMember as any)(ctx);
+
+  // Then
+  expect(tMock).toBeCalledWith(
+    'errors.remoteMemberRegisterSyntaxError',
+    expect.objectContaining({ mention: 'oi' }),
+  );
+  expect(replyWithAutoDestructiveMessageMock).toHaveBeenCalledWith('dummy');
+});
+
+it('replies with syntax error when command is issued with no locations', async () => {
+  // Prepare
+  const tMock = jest.fn().mockImplementation(() => 'dummy');
+  const replyWithAutoDestructiveMessageMock = jest.fn();
+  const ctx: DeepPartial<BotContext> = {
+    safeUser: { mention: undefined },
+    command: {},
+    i18n: { t: tMock },
+    replyWithAutoDestructiveMessage: replyWithAutoDestructiveMessageMock,
+  };
+
+  // Given
+  ctx.command = { args: '' };
+  ctx.safeUser = { mention: 'oi' };
+
+  // When
+  await (cmdRegisterRemoteMember as any)(ctx);
+
+  // Then
+  expect(tMock).toBeCalledWith(
+    'errors.remoteMemberRegisterSyntaxError',
+    expect.objectContaining({ mention: 'oi' }),
+  );
+  expect(replyWithAutoDestructiveMessageMock).toHaveBeenCalledWith('dummy');
+});
